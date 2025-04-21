ab5594e67719e2dda5fce1cf6f58cd1e121af514
Viktoriia46 <132668579+Viktoriia46@users.noreply.github.com>
2025-04-21 13:12:29 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-function sayHello(name: string) {
-    console.log("Hello, " + name);
+import React from 'react';
+
+function Hello({ name }: { name: string }) {
+  console.log('Hello, ' + name); // ESLint тут посвариться за console
+  const unused = 123; // і за це теж
+  return <div>Hello, {name}</div>;
 }
 
-sayHello("World");
-
-const unused = 42;
+export default Hello;
