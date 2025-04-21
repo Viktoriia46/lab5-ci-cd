84bb10116385a2902cda7874c0d59ef6ab6cc33c
Viktoriia <viktoriia.pshenychka@oa.edu.ua>
2025-04-21 14:40:48 +0300

    Add testing configuration with Vitest and Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/lab5-ci-cd/',
+  test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/SetupTest.tsx',
+  },
+});
+
